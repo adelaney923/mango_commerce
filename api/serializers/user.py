a036b09a703fb1ee65aafdbf8886d3bc8936ca0e
“Alex <alexandriadelaney923@gmail.com>
2021-12-14 11:34:04 -0800

    add mango model, serializer, and ran migration

--- api/serializers/user.py
+++ api/serializers/user.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from ..models.user import User
+# from ..models.user import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
